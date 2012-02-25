b0116a3c3b22e86b8078152cf2c087cae0ee9e31
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-02-25 19:00:16 -0200

    [Android] Add action for home icon in playlist
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/PlaylistActivity.java
+++ src/org/helllabs/android/xmp/PlaylistActivity.java
@@ -197,6 +197,11 @@ public abstract class PlaylistActivity extends ActionBarListActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, PlaylistMenu.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
 		case R.id.menu_new_playlist:
 			(new PlaylistUtils()).newPlaylist(this);
 			break;
