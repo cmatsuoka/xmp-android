5f98474dcab2d8ae5e1c71f37683d187a62605df
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-02-26 01:09:56 -0300

    [Android] Cosmetic changes
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/About.java
+++ src/org/helllabs/android/xmp/About.java
@@ -5,21 +5,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class About extends Activity {
-	
+
 	@Override
 	public void onCreate(Bundle icicle) {	
 		super.onCreate(icicle);
-		
+
 		setContentView(R.layout.about);
 
 		Xmp xmp = new Xmp();
-		
+
 		((TextView)findViewById(R.id.version_name))
 			.setText(getString(R.string.about_version, AppInfo.getVersion(this)));
-		
+
 		((TextView)findViewById(R.id.xmp_version))
 			.setText(getString(R.string.about_xmp, xmp.getVersion()));
 	}
-		
-
 }
