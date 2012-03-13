1539d582eeb4be93bb27fcf21f9926109d5b4a7b
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-13 19:23:49 -0300

    [Android] Change pattern viewer to be single-threaded
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/PatternViewer.java
+++ src/org/helllabs/android/xmp/PatternViewer.java
@@ -3,129 +3,107 @@ package org.helllabs.android.xmp;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.util.AttributeSet;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 
 // http://developer.android.com/guide/topics/graphics/2d-graphics.html
 
-public class PatternViewer extends SurfaceView implements SurfaceHolder.Callback {
+public class PatternViewer extends Viewer implements SurfaceHolder.Callback {
 	private Context context;
-	private PatternThread thread = null;
+	private static final byte noteMap[] = new byte[32 * 256];
 
-	class PatternThread extends Thread {
-        private SurfaceHolder surfaceHolder;        
-        private int canvasHeight, canvasWidth;
-        private Paint notePaint;
-        private Boolean running = false;
-        
-        public PatternThread(SurfaceHolder surfaceHolder, Context c) {
-        	this.surfaceHolder = surfaceHolder;
-        	context = c;
-        	
-        	notePaint = new Paint();
-        	notePaint.setARGB(255, 0, 255, 0);
-        }
+	//class PatternThread extends Thread {
+	private SurfaceHolder surfaceHolder;        
+	private int canvasHeight, canvasWidth;
+	private Paint notePaint, barPaint;
+	private int fontSize;
 
-		/* Callback invoked when the surface dimensions change. */
-		public void setSurfaceSize(int width, int height) {
-			// synchronized to make sure these all change atomically
+
+	/* Callback invoked when the surface dimensions change. */
+	public void setSurfaceSize(int width, int height) {
+		// synchronized to make sure these all change atomically
+		synchronized (surfaceHolder) {
+			Log.i("Xmp PatternViewer", "width=" + width + " height=" + height);
+			canvasWidth = width;
+			canvasHeight = height;
+		}
+	}
+
+	@Override
+	public void update() {
+		super.update();
+		
+		Canvas c = null;
+		try {
+			c = surfaceHolder.lockCanvas(null);
 			synchronized (surfaceHolder) {
-				Log.i("Xmp PatternViewer", "width=" + width + " height=" + height);
-				canvasWidth = width;
-				canvasHeight = height;
+				doDraw(c);
+			}
+		} finally {
+			// do this in a finally so that if an exception is thrown
+			// during the above, we don't leave the Surface in an
+			// inconsistent state
+			if (c != null) {
+				surfaceHolder.unlockCanvasAndPost(c);
 			}
 		}
-		
-		@Override
-        public void run() {
-            while (running) {
-            	//Log.i("asd", "is running");
-                Canvas c = null;
-                try {
-                    c = surfaceHolder.lockCanvas(null);
-                    synchronized (surfaceHolder) {
-                       doDraw(c);
-                    }
-				} finally {
-                    // do this in a finally so that if an exception is thrown
-                    // during the above, we don't leave the Surface in an
-                    // inconsistent state
-                    if (c != null) {
-                        surfaceHolder.unlockCanvasAndPost(c);
-                    }
-                }
-                try {
-					sleep(210);
-				} catch (InterruptedException e) {
+	}
 
-				}
-            }
-        }
-		
-		private void doDraw(Canvas canvas) {
-			canvas.drawLine(10, 10, 20, 20, notePaint);
-		}
 
-		public void setRunning(boolean b) {
-			running = b;
+	private void doDraw(Canvas canvas) {
+		int lines = canvasHeight / fontSize;
+		int barY = (lines / 2) * fontSize;
+		Rect rect;
+
+		//canvas.drawLine(10, 10, 20, 20, notePaint);
+		rect = new Rect(0, barY - fontSize, canvasWidth - 1, barY);
+
+		canvas.drawRect(rect, barPaint);
+		for (int i = 0; i < lines; i++) {
+			canvas.drawText("C#2 D 3 --- --- A 3 G#2 --- === --- C#7", 0, (i + 1) * fontSize, notePaint);
 		}
 	}
 
 
 	public PatternViewer(Context context) {
 		super(context);
-		
-		Log.d("Xmp PatternViewer", "PatternViewer constructor");
-		
-		// register our interest in hearing about changes to our surface
-        SurfaceHolder holder = getHolder();
-        holder.addCallback(this);
-        thread = new PatternThread(holder, context);
 
-		/*FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
-				FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
-		this.setLayoutParams(params);*/
+		Log.d("Xmp PatternViewer", "PatternViewer constructor");
+
+		// register our interest in hearing about changes to our surface
+		SurfaceHolder holder = getHolder();
+		holder.addCallback(this);
+
+		this.surfaceHolder = surfaceHolder;
+		this.context = context;
+
+		fontSize = 10;
+
+		notePaint = new Paint();
+		notePaint.setARGB(255, 140, 140, 160);
+		notePaint.setTypeface(Typeface.MONOSPACE);
+		notePaint.setTextSize(fontSize);
+
+		barPaint = new Paint();
+		barPaint.setARGB(255, 40, 40, 40);
 	}
 
-    // create thread only; it's started in surfaceCreated()
-
-    
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
-		thread.setSurfaceSize(width, height);
+		setSurfaceSize(width, height);
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
-		// start the thread here so that we don't busy-wait in run()
-		// waiting for the surface to be created
-
-		Log.d("Xmp PatternViewer", "surfaceCreated");
-		if (thread == null || !thread.isAlive()) {
-			thread =new PatternThread(holder, context);
-		}
-		thread.setRunning(true);
-		thread.start();
+		Log.d("Xmp PatternViewer", "surfaceCreated");		
+		surfaceHolder = holder;
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
-		// we have to tell thread to shut down & wait for it to finish, or else
-		// it might touch the Surface after we return and explode
-		
 		Log.d("Xmp PatternViewer", "surfaceDestroyed");
-		
-		boolean retry = true;
-		thread.setRunning(false);
-		while (retry) {
-			try {
-				thread.join();
-				retry = false;
-			} catch (InterruptedException e) {
-			}
-		}
 	}
 }
