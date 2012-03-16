0713ea23019c49543e1e6316e40e3c15a1babec3
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-16 09:52:14 -0300

    [Android] Reset ChannelView scroll on new module
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/ChannelViewer.java
+++ src/org/helllabs/android/xmp/ChannelViewer.java
@@ -16,16 +16,16 @@ public class ChannelViewer extends Viewer {
 	private int[] channelIns;
 	private Rect rect = new Rect();
 	private byte[] buffer;
-	private int[] holdKey = new int[64];
+	private int[] holdKey;
 	String channelNumber[];
 
 	@Override
 	public void setup(ModInterface modPlayer, int[] modVars) {
 		super.setup(modPlayer, modVars);
-		
+
 		int chn = modVars[3];
 		String[] instruments;
-		
+
 		try {
 			instruments = modPlayer.getInstruments();
 			for (int i = 0; i < 256; i++) {
@@ -36,30 +36,32 @@ public class ChannelViewer extends Viewer {
 				}
 			}
 		} catch (RemoteException e) { }
-		
+
 		channelIns = new int[chn];
 		for (int i = 0; i < chn; i++) {
 			channelIns[i] = -1;
 		}
-		
-		channelNumber = new String[chn];
-		for (int i = 0; i < chn; i++) {
-			channelNumber[i] = new String(String.format("%2d", i + 1));
-		}
-		
+
 		synchronized (isDown) {
 			posY = 0;
+		}
+
+		holdKey = new int[chn];
+		channelNumber = new String[chn];
+
+		for (int i = 0; i < chn; i++) {
+			channelNumber[i] = new String(String.format("%2d", i + 1));
 		}
 	}
 
 	@Override
 	public void update(Info info) {
 		Canvas c = null;
-		
+
 		try {
 			c = surfaceHolder.lockCanvas(null);
 			synchronized (surfaceHolder) {
-				
+
 				doDraw(c, modPlayer, info);
 			}
 		} finally {
@@ -83,24 +85,24 @@ public class ChannelViewer extends Viewer {
 		final int panLeft = volLeft + volWidth + 4 * fontWidth;
 		final int panWidth = volWidth;
 		int biasY;
-		
+
 		synchronized (isDown) {
 			int max = canvasHeight - (chn * 4 + 1) * fontHeight;
 			biasY = deltaY + posY;
-			
+
 			if (max > 0) {
 				max = 0;
 			}
-			
+
 			if (biasY > 0) {
 				biasY = posY = 0;
 			}
-			
+
 			if (biasY < max) {
 				biasY = max;
 			}
 		}
-		
+
 		// Clear screen
 		canvas.drawColor(Color.BLACK);
 
@@ -111,35 +113,34 @@ public class ChannelViewer extends Viewer {
 			final int pan = info.pans[i];
 			int key = info.keys[i];
 			final int period = info.periods[i];
-			
+
 			if (y > canvasHeight) {
 				continue;
 			}
 
 			// Draw channel number
-			
 			canvas.drawText(channelNumber[i], 0, y + scopeHeight / 2 + font2Height / 2, numPaint);
-			
+
 			// Draw scopes
-			
+
 			rect.set(scopeLeft, y + 1, scopeLeft + scopeWidth, y + scopeHeight);
 			canvas.drawRect(rect, scopePaint);
-			
+
 			if (key >= 0) {
 				holdKey[i] = key;
 			} else {
 				key = holdKey[i];
 			}
-			
+
 			try {
-				
+
 				// Be very careful here!
 				// Our variables are latency-compensated but sample data is current
 				// so caution is needed to avoid retrieving data using old variables
 				// from a module with sample data from a newly loaded one.
-				
+
 				modPlayer.getSampleData(ins, key, period, scopeWidth, buffer);
-				
+
 			} catch (RemoteException e) { }
 			for (int j = 0; j < scopeWidth; j++) {
 				canvas.drawPoint(scopeLeft + j, y + scopeHeight / 2 + buffer[j] * scopeHeight / 2 / 180, scopeLinePaint);
@@ -148,16 +149,16 @@ public class ChannelViewer extends Viewer {
 			if (ins >= 0) {
 				channelIns[i] = ins;
 			}
-			
+
 			int ci = channelIns[i];
 			if (ci >= 0) {
 				canvas.drawText(insName[ci + 1], volLeft, y + fontHeight, insPaint);
 			}
-			
+
 			// Draw volumes
 			int volX = volLeft + vol * volWidth / volBase;
 			int volY1 = y + 2 * fontHeight;
-			int volY2 = y + 2 * fontHeight + fontHeight / 3;
+			int volY2 = y + 2 * fontHeight + fontHeight / 3;	
 			rect.set(volLeft, volY1, volX, volY2);
 			canvas.drawRect(rect, meterPaint);
 			rect.set(volX + 1, volY1, volLeft + volWidth, volY2);
@@ -171,40 +172,40 @@ public class ChannelViewer extends Viewer {
 			canvas.drawRect(rect, meterPaint);
 		}
 	}
-	
+
 	public ChannelViewer(Context context) {
 		super(context);
-		
+
 		fontSize = getResources().getDimensionPixelSize(R.dimen.channelview_font_size);
 		font2Size = getResources().getDimensionPixelSize(R.dimen.channelview_channel_font_size);
-		
+
 		scopePaint = new Paint();
 		scopePaint.setARGB(255, 40, 40, 40);
-		
+
 		scopeLinePaint = new Paint();
 		scopeLinePaint.setARGB(255, 80, 160, 80);
-		
+
 		meterPaint = new Paint();
 		meterPaint.setARGB(255, 40, 80, 160);
-		
+
 		insPaint = new Paint();
 		insPaint.setARGB(255, 140, 140, 160);
 		insPaint.setTypeface(Typeface.MONOSPACE);
 		insPaint.setTextSize(fontSize);
 		insPaint.setAntiAlias(true);
-		
+
 		numPaint = new Paint();
 		numPaint.setARGB(255, 220, 220, 220);
 		numPaint.setTypeface(Typeface.MONOSPACE);
 		numPaint.setTextSize(font2Size);
 		numPaint.setAntiAlias(true);
-		
+
 		fontWidth = (int)insPaint.measureText("X");
 		fontHeight = fontSize * 12 / 10;
 
 		font2Width = (int)numPaint.measureText("X");
 		font2Height = font2Size * 12 / 10;
-		
+
 		buffer = new byte[8 * fontWidth];
 	}
 }
