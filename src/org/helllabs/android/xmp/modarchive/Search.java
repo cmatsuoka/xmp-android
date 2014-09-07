1a6a9bbcacbc786e9af2f490f908e4c27e4e2f02
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-09-07 11:34:24 -0300

    Continue implementation of download activity
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/modarchive/Search.java
+++ src/org/helllabs/android/xmp/modarchive/Search.java
@@ -4,8 +4,36 @@ import org.helllabs.android.xmp.R;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class Search extends ActionBarActivity {
+	
+	private RadioGroup searchType;
+	
+	private final View.OnClickListener searchClick = new View.OnClickListener() {
+		@Override
+		public void onClick(final View view) {
+			final int selectedId = searchType.getCheckedRadioButtonId();
+			
+			switch (selectedId) {
+			case R.id.title_radio:
+				break;
+			case R.id.artist_radio:
+				break;
+			default:
+				break;
+			}
+		}
+	};
+	
+	private final View.OnClickListener randomClick = new View.OnClickListener() {
+		@Override
+		public void onClick(final View view) {
+			
+		}
+	};
 	
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
@@ -13,6 +41,15 @@ public class Search extends ActionBarActivity {
 		setContentView(R.layout.search);
 		
 		setTitle("Module search");
+		
+		final Button searchButton = (Button)findViewById(R.id.search_button);
+		final Button randomButton = (Button)findViewById(R.id.random_button);
+		
+		searchType = (RadioGroup)findViewById(R.id.search_type);
+		searchType.check(R.id.title_radio);
+		
+		searchButton.setOnClickListener(searchClick);
+		randomButton.setOnClickListener(randomClick);
 	}
 
 }
