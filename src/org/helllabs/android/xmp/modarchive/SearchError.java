befc76e312efe833101fac826eed17d585f5f19a
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-09-28 13:26:32 -0300

    Use guru-styled volley error message
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/modarchive/SearchError.java
+++ src/org/helllabs/android/xmp/modarchive/SearchError.java
@@ -8,7 +8,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
 import android.widget.TextView;
 
-public class SearchError extends ActionBarActivity {
+public class SearchError extends ActionBarActivity implements Runnable {
+	
+	private static final int PERIOD = 1337;
+	
+	private TextView msg;
+	private boolean frameBlink;
 	
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
@@ -18,8 +23,17 @@ public class SearchError extends ActionBarActivity {
 		setTitle("Search error");
 		
 		final Throwable error = (Throwable)getIntent().getSerializableExtra(Search.ERROR);
-		final TextView msg = (TextView)findViewById(R.id.error_message);
+		msg = (TextView)findViewById(R.id.error_message);
 		msg.setText(error.getMessage());
+		
+		msg.postDelayed(this, PERIOD);
+	}
+	
+	
+	@Override
+	public void onDestroy() {
+		msg.removeCallbacks(this);
+		super.onDestroy();
 	}
 	
 	@Override
@@ -33,5 +47,15 @@ public class SearchError extends ActionBarActivity {
 	        return true;
 	    }
 	    return super.onKeyDown(keyCode, event);
+	}
+
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void run() {
+		// Guru frame blink
+		msg.setBackgroundDrawable(getResources().getDrawable(frameBlink ? R.drawable.guru_frame : R.drawable.guru_frame_2));
+		frameBlink ^= true;
+		msg.postDelayed(this, PERIOD);		
 	}
 }
