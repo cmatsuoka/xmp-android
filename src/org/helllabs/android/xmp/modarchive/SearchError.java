077958c28540bc9605291060ce9d2613f6805a21
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-09-28 12:32:09 -0300

    Skip result when returning from search error activity
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/modarchive/SearchError.java
+++ src/org/helllabs/android/xmp/modarchive/SearchError.java
@@ -2,8 +2,10 @@ package org.helllabs.android.xmp.modarchive;
 
 import org.helllabs.android.xmp.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 public class SearchError extends ActionBarActivity {
@@ -18,5 +20,18 @@ public class SearchError extends ActionBarActivity {
 		final Throwable error = (Throwable)getIntent().getSerializableExtra(Search.ERROR);
 		final TextView msg = (TextView)findViewById(R.id.error_message);
 		msg.setText(error.getMessage());
+	}
+	
+	@Override
+	public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+		
+		// Back key returns to search
+	    if (keyCode == KeyEvent.KEYCODE_BACK) {
+	        final Intent intent = new Intent(this, Search.class);
+	        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	        startActivity(intent);
+	        return true;
+	    }
+	    return super.onKeyDown(keyCode, event);
 	}
 }
