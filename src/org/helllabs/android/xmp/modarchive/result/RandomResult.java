594152da68ca99ad98bfbdde3c11c528486c0c55
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-09-29 13:23:38 -0300

    Prepare to handle random pick errors
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/modarchive/result/RandomResult.java
+++ src/org/helllabs/android/xmp/modarchive/result/RandomResult.java
@@ -1,15 +1,27 @@
 package org.helllabs.android.xmp.modarchive.result;
 
+import java.util.List;
+
 import org.helllabs.android.xmp.R;
+import org.helllabs.android.xmp.modarchive.model.Module;
 import org.helllabs.android.xmp.modarchive.request.ModuleRequest;
 
 
-public class RandomResult extends ModuleResult {
+public class RandomResult extends ModuleResult implements ModuleRequest.OnResponseListener<List<Module>> {
 
 	@Override
 	protected void makeRequest(final String query) {
 		final String key = getString(R.string.modarchive_apikey);
 		final ModuleRequest request = new ModuleRequest(key, "random");
 		request.setOnResponseListener(this).send();
+	}
+	
+	@Override
+	public void onResponse(final List<Module> moduleList) {
+		if (moduleList.size() > 0) {
+			super.onResponse(moduleList);
+		} else {
+			// Handle error!
+		}
 	}
 }
