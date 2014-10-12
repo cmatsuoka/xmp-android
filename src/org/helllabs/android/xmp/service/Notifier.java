4d9c6a71a89d70d4b0d9f7b1890610305471b3d7
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-10-12 19:01:35 -0300

    Service notification code cleanup
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/org/helllabs/android/xmp/service/Notifier.java
+++ src/org/helllabs/android/xmp/service/Notifier.java
@@ -1,69 +1,48 @@
 package org.helllabs.android.xmp.service;
 
+import java.util.Locale;
+
 import org.helllabs.android.xmp.R;
 import org.helllabs.android.xmp.player.PlayerActivity;
 import org.helllabs.android.xmp.service.receiver.NotificationActionReceiver;
 
-import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 
 
-@SuppressLint("DefaultLocale")
 public class Notifier {
-	//private final NotificationManager nm;
     private final PendingIntent contentIntent;
     private static final int NOTIFY_ID = R.layout.player;
     public static final String ACTION_STOP = "org.helllabs.android.xmp.STOP";
     public static final String ACTION_PAUSE = "org.helllabs.android.xmp.PAUSE";
     public static final String ACTION_NEXT = "org.helllabs.android.xmp.NEXT";
 	private QueueManager queue;
-	private final Context context;
+	private final Service service;
 	private final long when;
 
-	public Notifier(final Context context) {
-    	this.context = context;
-		//nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-    	contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, PlayerActivity.class), 0);
+	public Notifier(final Service service) {
+    	this.service = service;
+    	final Intent intent = new Intent(service, PlayerActivity.class);
+    	contentIntent = PendingIntent.getActivity(service, 0, intent, 0);
     	when = System.currentTimeMillis();
 	}
 	
 	private String formatIndex(final int index) {
-		return String.format("%d/%d", index + 1, queue.size());
+		return String.format(Locale.US, "%d/%d", index + 1, queue.size());
 	}
 	
 	public void cancel() {
-		//nm.cancel(NOTIFY_ID);
-		((Service)context).stopForeground(true);
+		service.stopForeground(true);
 	}
 	
-	//public void clean() {
-	//	notification(null);
-	//}
-	
-	// Simple one-line notification for player messages
-//	public void notification(final String title) {			
-//		final Bitmap icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon);
-//		
-//		final Notification notification = new NotificationCompat.Builder(context)
-//				.setTicker(title)
-//				.setSmallIcon(R.drawable.notification_icon)
-//				.setLargeIcon(icon)
-//				.build();
-//		
-//		((Service)context).startForeground(NOTIFY_ID, notification);
-//		//nm.notify(NOTIFY_ID, notification);
-//	}
-	
 	private PendingIntent makePendingIntent(final String action) {
-		final Intent intent = new Intent(context, NotificationActionReceiver.class);
+		final Intent intent = new Intent(service, NotificationActionReceiver.class);
 		intent.setAction(action);
-		return PendingIntent.getBroadcast(context, 0, intent, 0);
+		return PendingIntent.getBroadcast(service, 0, intent, 0);
 	}
 	
 	public void tickerNotification(final String title, final int index) {
@@ -80,7 +59,7 @@ public class Notifier {
 		
 	// Notification with player buttons
 	private void notification(String title, final int index, final boolean ticker, final boolean paused) {
-		final Bitmap icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon);
+		final Bitmap icon = BitmapFactory.decodeResource(service.getResources(), R.drawable.icon);
 		
 		if (title != null && title.trim().isEmpty()) {
 			title = "<untitled>";
@@ -91,8 +70,8 @@ public class Notifier {
 		final PendingIntent nextIntent = makePendingIntent(ACTION_NEXT);
 		final String indexText = formatIndex(index);
 		
-		final NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-				.setContentTitle(context.getString(R.string.app_name))
+		final NotificationCompat.Builder builder = new NotificationCompat.Builder(service)
+				.setContentTitle(service.getString(R.string.app_name))
 				.setContentInfo(indexText)
 				.setContentIntent(contentIntent)
 				.setSmallIcon(R.drawable.notification_icon)
@@ -124,8 +103,7 @@ public class Notifier {
 		
 		builder.addAction(R.drawable.ic_action_next, "Next", nextIntent);
 				
-		((Service)context).startForeground(NOTIFY_ID, builder.build());
-		//nm.notify(NOTIFY_ID, notification);
+		service.startForeground(NOTIFY_ID, builder.build());
 	}
 	
 	public void setQueue(final QueueManager queue) {
