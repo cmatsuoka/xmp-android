c9fc7c4210c6fde494685f851a963d607f6408ad
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-04-20 09:13:50 -0300

    Moved project from Eclipse to Android Studio
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,12 +3,12 @@ LOCAL_PATH	:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := xmp-prebuilt
-LOCAL_SRC_FILES := ../../../libxmp/obj/local/$(TARGET_ARCH_ABI)/libxmp.a
+LOCAL_SRC_FILES := ../../../../../../libxmp/obj/local/$(TARGET_ARCH_ABI)/libxmp.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE	:= xmp-jni
-LOCAL_CFLAGS    := -O3 -I$(LOCAL_PATH)/../../../libxmp/include \
+LOCAL_CFLAGS    := -O3 -I$(LOCAL_PATH)/../../../../../../libxmp/include \
                    -Wno-int-to-pointer-cast -Wno-pointer-to-int-cast
 LOCAL_STATIC_LIBRARIES := xmp-prebuilt
 LOCAL_SRC_FILES := xmp-jni.c opensl.c
