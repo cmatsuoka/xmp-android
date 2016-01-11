f95eaa5b09a0eeb0c01bc173d8dbe2f42e5ffad2
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-01-10 22:06:12 -0200

    Remove unused imports
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- app/src/main/java/org/helllabs/android/xmp/browser/FilelistActivity.java
+++ app/src/main/java/org/helllabs/android/xmp/browser/FilelistActivity.java
@@ -21,7 +21,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
--- app/src/main/java/org/helllabs/android/xmp/browser/PlaylistActivity.java
+++ app/src/main/java/org/helllabs/android/xmp/browser/PlaylistActivity.java
@@ -3,7 +3,6 @@ package org.helllabs.android.xmp.browser;
 import android.graphics.drawable.NinePatchDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
